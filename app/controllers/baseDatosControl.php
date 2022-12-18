0454a32694050379921c9db703a3ab59a9ada98a
jefe <jefe@daw.es>
2022-12-19 00:50:45 +0100

    commit

--- app/controllers/baseDatosControl.php
+++ app/controllers/baseDatosControl.php
@@ -3,16 +3,11 @@
 /**
  * listar: genera un array con cada operario y su id para listarlos en el select del formulario
  * 
- * @return void
+ * @return array
  */
 function listar()
 {
     require(APP_PATH . "models/baseDatosUsuariosModel.php");
     $usuarios = new Usuarios();
-    $operarios = $usuarios->getOperarios();
-    //include("tareasControl.php");
+    return $operarios = $usuarios->getOperarios();
 }
-/*require("../models/baseDatosUsuarios.php");
-$usuarios = new Usuarios();
-$operarios = $usuarios->getOperarios();
-include("tareasControl.php");*/
