6ea02cb9270cee1170fe69740e66946021cdb769
jefe <jefe@daw.es>
2022-12-20 16:31:39 +0100

    blade configuracion

---
+++ app/constantes.php
@@ -0,0 +1,17 @@
+<?php
+// Defino la URL en la que se encuentra la aplicación
+define('BASE_URL', 'http://localhost:3000/Users/david/Desktop/FP/2º%20DAW%20(Curso%2022-23)/Desarrollo%20Web%20en%20Entorno%20Servidor/Proyecto1Ev/app/');
+
+// Defino la ruta raiz
+define('APP_PATH', __DIR__ . "/");
+
+// Defino constante de la carpeta de controladores
+define('CONTROLLERS_FOLDER', APP_PATH . "controllers/");
+
+// Defino constante de controlador por defecto
+
+define('DEFAULT_CONTROLLER', "tareas");
+
+// Defino constante de la acción por defecto
+
+define('DEFAULT_ACTION', "iniciar");
--- app/models/varios.php
+++ app/models/varios.php
@@ -1,14 +1,15 @@
 <?php
+require __DIR__ . '/../constantes.php';
 
 use Jenssegers\Blade\Blade;
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-define('MODEL_PATH', __DIR__ . '/app/models/');
-define('CACHE_PATH', __DIR__ . '/app/views/cache/');
-define('CTRL_PATH', __DIR__ . '/app/controllers/');
+// define('MODEL_PATH', __DIR__ . '/app/models/');
+define('CACHE_PATH', APP_PATH . 'views/cache/');
+// define('CTRL_PATH', __DIR__ . '/app/controllers/');
 define('VIEW_PATH', __DIR__ . '/app/views/');
-
+echo (CACHE_PATH);
 include(__DIR__ . '/../../vendor/autoload.php');
 
 $blade = new Blade(VIEW_PATH, CACHE_PATH);
